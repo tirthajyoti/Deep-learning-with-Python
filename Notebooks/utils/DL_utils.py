089af1ec2b25b7f3a1d57b37b2461d74b148bfb4
Tirthajyoti Sarkar <tirthajyoti@gmail.com>
2019-09-15 21:22:11 -0700

    Updated predict functions and added preprocess_image

--- Notebooks/utils/DL_utils.py
+++ Notebooks/utils/DL_utils.py
@@ -173,3 +173,185 @@ def plot_loss_acc(model,target_acc=0.9, title=None):
     plt.title(title)
   plt.hlines(y=target_acc,xmin=1,xmax=e.max(),colors='k', linestyles='dashed',lw=3)
   plt.show()
+
+
+def train_CNN(train_directory,target_size=(256,256), 
+              classes=None,batch_size=128,num_classes=2,
+              num_epochs=20,verbose=0):
+    """
+    Trains a conv net for the flowers dataset with a 5-class classifiction output
+    Also provides suitable arguments for extending it to other similar apps
+    
+    Arguments:
+            train_directory: The directory where the training images are stored in separate folders.
+                            These folders should be named as per the classes.
+            target_size: Target size for the training images. A tuple e.g. (200,200)
+            classes: A Python list with the classes 
+            batch_size: Batch size for training
+            num_epochs: Number of epochs for training
+            num_classes: Number of output classes to consider
+            verbose: Verbosity level of the training, passed on to the `fit_generator` method
+    Returns:
+            A trained conv net model
+    
+    """
+    from tensorflow.keras.preprocessing.image import ImageDataGenerator
+    import tensorflow as tf
+    from tensorflow.keras.optimizers import RMSprop
+    
+    # ImageDataGenerator object instance with scaling
+    train_datagen = ImageDataGenerator(rescale=1/255)
+
+    # Flow training images in batches using the generator
+    train_generator = train_datagen.flow_from_directory(
+            train_directory,  # This is the source directory for training images
+            target_size=target_size,  # All images will be resized to 200 x 200
+            batch_size=batch_size,
+            # Specify the classes explicitly
+            classes = classes,
+            # Since we use categorical_crossentropy loss, we need categorical labels
+            class_mode='categorical')
+    
+    input_shape = tuple(list(target_size)+[3])
+    
+    # Model architecture
+    model = tf.keras.models.Sequential([
+    # Note the input shape is the desired size of the image 200x 200 with 3 bytes color
+    # The first convolution
+    tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=input_shape),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    # The second convolution
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The third convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The fourth convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The fifth convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # Flatten the results to feed into a dense layer
+    tf.keras.layers.Flatten(),
+    # 512 neuron in the fully-connected layer
+    tf.keras.layers.Dense(512, activation='relu'),
+    # Output neurons for `num_classes` classes with the softmax activation
+    tf.keras.layers.Dense(num_classes, activation='softmax')
+    ])
+    
+    # Optimizer and compilation
+    model.compile(loss='categorical_crossentropy',
+              optimizer=RMSprop(lr=0.001),
+              metrics=['acc'])
+    
+    # Total sample count
+    total_sample=train_generator.n
+    
+    # Training
+    model.fit_generator(
+        train_generator, 
+        steps_per_epoch=int(total_sample/batch_size),  
+        epochs=num_epochs,
+        verbose=verbose)
+    
+    return model
+
+def preprocess_image(img_path,model=None,rescale=255,resize=(256,256)):
+    """
+    Preprocesses a given image for prediction with a trained model, with rescaling and resizing options
+    
+    Arguments:
+            img_path: The path to the image file
+            rescale: A float or integer indicating required rescaling. 
+                    The image array will be divided (scaled) by this number.
+            resize: A tuple indicating desired target size. 
+                    This should match the input shape as expected by the model
+    Returns:
+            img: A processed image.
+    """
+    from keras.preprocessing.image import img_to_array, load_img
+    import cv2
+    import numpy as np
+    
+    assert type(img_path)==str, "Image path must be a string"
+    assert type(rescale)==int or type(rescale)==float, "Rescale factor must be either a float or int"
+    assert type(resize)==tuple and len(resize)==2, "Resize target must be a tuple with two elements"
+    
+    img = load_img(img_path)
+    img = img_to_array(img)
+    img = img/float(rescale)
+    img = cv2.resize(img,resize)
+    if model!=None:
+        if len(model.input_shape)==4:
+            img = np.expand_dims(img,axis=0)
+        
+    return img
+
+def pred_prob_with_model(img_path,model,rescale=255,resize=(256,256)):
+    """
+    Tests a given image with a trained model, with rescaling and resizing options
+    
+    Arguments:
+            img_path: The path to the image file
+            model: The trained Keras model
+            rescale: A float or integer indicating required rescaling. 
+                    The image array will be divided (scaled) by this number.
+            resize: A tuple indicating desired target size. 
+                    This should match the input shape as expected by the model
+    Returns:
+            pred: A prediction vector (Numpy array).
+                  Could be either classes or probabilities depending on the model.
+    """
+    from keras.preprocessing.image import img_to_array, load_img
+    import cv2
+    
+    assert type(img_path)==str, "Image path must be a string"
+    assert type(rescale)==int or type(rescale)==float, "Rescale factor must be either a float or int"
+    assert type(resize)==tuple and len(resize)==2, "Resize target must be a tuple with two elements"
+    
+    img = load_img(img_path)
+    img = img_to_array(img)
+    img = img/float(rescale)
+    img = cv2.resize(img,resize)
+    if len(model.input_shape)==4:
+        img = np.expand_dims(img,axis=0)
+    
+    pred = model.predict(img)
+        
+    return pred
+
+def pred_class_with_model(img_path,model,rescale=255,resize=(256,256)):
+    """
+    Tests a given image with a trained model, with rescaling and resizing options
+    
+    Arguments:
+            img_path: The path to the image file
+            model: The trained Keras model
+            rescale: A float or integer indicating required rescaling. 
+                    The image array will be divided (scaled) by this number.
+            resize: A tuple indicating desired target size. 
+                    This should match the input shape as expected by the model
+    Returns:
+            pred: A prediction vector (Numpy array).
+                  Could be either classes or probabilities depending on the model.
+    """
+    from keras.preprocessing.image import img_to_array, load_img
+    import cv2
+    
+    assert type(img_path)==str, "Image path must be a string"
+    assert type(rescale)==int or type(rescale)==float, "Rescale factor must be either a float or int"
+    assert type(resize)==tuple and len(resize)==2, "Resize target must be a tuple with two elements"
+    
+    img = load_img(img_path)
+    img = img_to_array(img)
+    img = img/float(rescale)
+    img = cv2.resize(img,resize)
+    if len(model.input_shape)==4:
+        img = np.expand_dims(img,axis=0)
+    
+    pred = model.predict(img)
+    pred_class = pred.argmax(axis=-1)
+        
+    return pred_class
+    
